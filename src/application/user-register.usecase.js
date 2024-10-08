5cb2bcabca30ce50fa75c4d6099f62fcbb85575e
Evandro Calado <evandrocalado07@gmail.com>
2024-10-08 09:17:53 -0300

    feat: ensure throw error if userRepository not ptovider

--- src/application/user-register.usecase.js
+++ src/application/user-register.usecase.js
@@ -1,4 +1,8 @@
 module.exports = function userRegisterUseCase({ userRepository }) {
+  if (!userRepository) {
+    throw new Error('UserRepository is required');
+  }
+
   return async function ({ name, cpf, phone, address, email }) {
     await userRepository.register({ name, cpf, phone, address, email });
   };
