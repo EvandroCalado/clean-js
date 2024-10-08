7e9a2c1de9b2ffba6069e858c4aafef8d3506a23
Evandro Calado <evandrocalado07@gmail.com>
2024-10-08 09:32:18 -0300

    test: create AppError tests

--- src/application/user-register.usecase.js
+++ src/application/user-register.usecase.js
@@ -1,6 +1,8 @@
+const AppError = require('../shared/errors/AppError');
+
 module.exports = function userRegisterUseCase({ userRepository }) {
   if (!userRepository) {
-    throw new Error('UserRepository is required');
+    throw new AppError(AppError.dependencyError);
   }
 
   return async function ({ name, cpf, phone, address, email }) {
