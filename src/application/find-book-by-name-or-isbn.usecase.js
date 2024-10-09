42b3aa6b5c434db2cb03ac85635afd5f6bcd9d67
Evandro Calado <evandrocalado07@gmail.com>
2024-10-09 09:53:46 -0300

    feat: throw AppError if bookRepository not provides

--- src/application/find-book-by-name-or-isbn.usecase.js
+++ src/application/find-book-by-name-or-isbn.usecase.js
@@ -1,6 +1,8 @@
-const { Either } = require('../shared/errors');
+const { Either, AppError } = require('../shared/errors');
 
 module.exports = function findBookByNameOrIsbnUseCase({ bookRepository }) {
+  if (!bookRepository) throw new AppError(AppError.dependencyError);
+
   return async function ({ value }) {
     const books = await bookRepository.findByNameOrIsbn(value);
 
