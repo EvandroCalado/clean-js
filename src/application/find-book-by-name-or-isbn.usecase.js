86ca5867f8a6619e84551fb072428efb16a6508e
Evandro Calado <evandrocalado07@gmail.com>
2024-10-09 10:04:04 -0300

    feat: throw AppError if name or isbn not provides

--- src/application/find-book-by-name-or-isbn.usecase.js
+++ src/application/find-book-by-name-or-isbn.usecase.js
@@ -4,6 +4,8 @@ module.exports = function findBookByNameOrIsbnUseCase({ bookRepository }) {
   if (!bookRepository) throw new AppError(AppError.dependencyError);
 
   return async function ({ value }) {
+    if (!value) throw new AppError(AppError.missingParamsError);
+
     const books = await bookRepository.findByNameOrIsbn(value);
 
     return Either.right(books);
