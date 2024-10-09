9560d3ba8d02705c68596082def4bcb73494bf64
Evandro Calado <evandrocalado07@gmail.com>
2024-10-09 08:39:27 -0300

    feat: throw AppError if all fields required is not provider

--- src/application/book-register.usecase.js
+++ src/application/book-register.usecase.js
@@ -1,7 +1,17 @@
-const { Either } = require('../shared/errors');
+const { Either, AppError } = require('../shared/errors');
 
 module.exports = function bookRegisterUseCase({ bookRepository }) {
+  if (!bookRepository) throw new AppError(AppError.dependencyError);
+
   return async function ({ name, quantity, author, genre, isbn }) {
+    const checkFields = [name, quantity, author, genre, isbn];
+
+    for (const field of checkFields) {
+      if (field === undefined) {
+        throw new AppError(AppError.missingParamsError);
+      }
+    }
+
     await bookRepository.register({ name, quantity, author, genre, isbn });
 
     return Either.right(null);
