c45b785f7b60bd9f6ce992e0421a06e04aa887b9
Evandro Calado <evandrocalado07@gmail.com>
2024-10-09 09:53:58 -0300

    test: throw AppError if bookRepository not provides

--- src/application/find-book-by-name-or-isbn.usecase.spec.js
+++ src/application/find-book-by-name-or-isbn.usecase.spec.js
@@ -1,3 +1,4 @@
+const { AppError } = require('../shared/errors');
 const findBookByNameOrIsbnUseCase = require('./find-book-by-name-or-isbn.usecase');
 
 describe('findBookByNameOrIsbnUseCase', () => {
@@ -27,5 +28,24 @@ describe('findBookByNameOrIsbnUseCase', () => {
     expect(output.right).toEqual(outputDTO);
     expect(bookRepository.findByNameOrIsbn).toHaveBeenCalledWith(nameIsbnDTO.value);
     expect(bookRepository.findByNameOrIsbn).toHaveBeenCalledTimes(1);
+  });
+
+  it('should return aarray empty if name or isbn is not registered', async () => {
+    bookRepository.findByNameOrIsbn.mockResolvedValue([]);
+
+    const nameIsbnDTO = {
+      value: 'invalid_name or invalid_isbn',
+    };
+
+    const sut = findBookByNameOrIsbnUseCase({ bookRepository });
+    const output = await sut(nameIsbnDTO);
+
+    expect(output.right).toEqual([]);
+    expect(bookRepository.findByNameOrIsbn).toHaveBeenCalledWith(nameIsbnDTO.value);
+    expect(bookRepository.findByNameOrIsbn).toHaveBeenCalledTimes(1);
+  });
+
+  it('should return a throw AppError if book repository not provider', () => {
+    expect(() => findBookByNameOrIsbnUseCase({})).toThrow(new AppError(AppError.dependencyError));
   });
 });
