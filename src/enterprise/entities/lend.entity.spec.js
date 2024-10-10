9a2a546c7771d234299ae596c74b6ce2dba6ad13
Evandro Calado <evandrocalado07@gmail.com>
2024-10-10 10:06:12 -0300

    test: ensure generate late fee on returned book before return date

--- src/enterprise/entities/lend.entity.spec.js
+++ src/enterprise/entities/lend.entity.spec.js
@@ -4,9 +4,18 @@ describe('lendEntity', () => {
   it('should not generate late fee', () => {
     const result = lendEntity.calculateLateFee({
       returnDate: '2024-10-11',
-      returnedDate: '2024-10-10',
+      returnedDate: '2024-10-11',
     });
 
     expect(result).toBe('Late fee: $0.00');
+  });
+
+  it('should generate late fee', () => {
+    const result = lendEntity.calculateLateFee({
+      returnDate: '2024-10-11',
+      returnedDate: '2024-10-12',
+    });
+
+    expect(result).toBe('Late fee: $10.00');
   });
 });
