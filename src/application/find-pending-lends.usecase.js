18f473e0277550780d9cda0afcf455236a2493b4
Evandro Calado <evandrocalado07@gmail.com>
2024-10-10 19:43:21 -0300

    refactor: change file name

--- src/application/find-pending-lends.usecase.js
+++ src/application/find-pending-lends.usecase.js
@@ -1,10 +1,10 @@
 const { AppError, Either } = require('../shared/errors');
 
-module.exports = function findLendUseCase({ lendRepository }) {
+module.exports = function findPendingLendsUseCase({ lendRepository }) {
   if (!lendRepository) throw new AppError(AppError.dependencyError);
 
   return async function () {
-    const lends = await lendRepository.findPendingBooks();
+    const lends = await lendRepository.findPendingLends();
     return Either.right(lends);
   };
 };
