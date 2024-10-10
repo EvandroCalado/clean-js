dbed77ce07f5e3000059ac85cba12ed3983ba2f4
Evandro Calado <evandrocalado07@gmail.com>
2024-10-10 09:29:18 -0300

    test: gererate late fee if returned date before return date

--- src/application/return-book.usecase.spec.js
+++ src/application/return-book.usecase.spec.js
@@ -6,15 +6,37 @@ describe('returnBookUseCase', () => {
   };
 
   it('should return book without late fee', async () => {
+    lendRepository.return.mockResolvedValue({
+      returnDate: new Date('2024-10-11'),
+    });
+
     const returnBookDTO = {
       lendId: 'valid_lendId',
-      returnDate: new Date('2024-10-10'),
+      returnedDate: new Date('2024-10-10'),
     };
 
     const sut = returnBookUseCase({ lendRepository });
     const output = await sut(returnBookDTO);
 
     expect(output.right).toBe('Late fee: $0.00');
+    expect(lendRepository.return).toHaveBeenCalledWith(returnBookDTO);
+    expect(lendRepository.return).toHaveBeenCalledTimes(1);
+  });
+
+  it('should return book with late fee', async () => {
+    lendRepository.return.mockResolvedValue({
+      returnDate: new Date('2024-10-09'),
+    });
+
+    const returnBookDTO = {
+      lendId: 'valid_lendId',
+      returnedDate: new Date('2024-10-10'),
+    };
+
+    const sut = returnBookUseCase({ lendRepository });
+    const output = await sut(returnBookDTO);
+
+    expect(output.right).toBe('Late fee: $10.00');
     expect(lendRepository.return).toHaveBeenCalledWith(returnBookDTO);
     expect(lendRepository.return).toHaveBeenCalledTimes(1);
   });
