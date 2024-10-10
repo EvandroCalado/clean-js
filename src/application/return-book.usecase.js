a4343e31d5622bf9bef14434a9bf34c90daf42b5
Evandro Calado <evandrocalado07@gmail.com>
2024-10-10 09:28:53 -0300

    feat: gererate late fee if returned date before return date

--- src/application/return-book.usecase.js
+++ src/application/return-book.usecase.js
@@ -1,10 +1,11 @@
 const { Either } = require('../shared/errors');
 
 module.exports = function returnBookUseCase({ lendRepository }) {
-  return async function ({ lendId, returnDate }) {
-    await lendRepository.return({ lendId, returnDate });
+  return async function ({ lendId, returnedDate }) {
+    const { returnDate } = await lendRepository.return({ lendId, returnedDate });
 
-    const verifyLateFee = 'Late fee: $0.00';
+    const verifyReturnDate = new Date(returnDate).getTime() < new Date(returnedDate).getTime();
+    const verifyLateFee = verifyReturnDate ? 'Late fee: $10.00' : 'Late fee: $0.00';
 
     return Either.right(verifyLateFee);
   };
