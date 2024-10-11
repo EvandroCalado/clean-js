5384458d463cfec5ae6adc02fc3253989341fe83
Evandro Calado <evandrocalado07@gmail.com>
2024-10-10 23:39:01 -0300

    test: ensure return a user if send a valid cpf on findByCpf

--- src/infra/db/typeorm/repositories/user.repository.spec.js
+++ src/infra/db/typeorm/repositories/user.repository.spec.js
@@ -1,6 +1,11 @@
-const { userRepository } = require('./user.repository');
+const { typeormServer } = require('../setup');
+const { userRepository, typeormUserRepository } = require('./user.repository');
 
 describe('userRepository', () => {
+  beforeEach(async () => {
+    await typeormUserRepository.delete({});
+  });
+
   it('should return void when registered a user', async () => {
     const sut = userRepository();
 
@@ -13,5 +18,22 @@ describe('userRepository', () => {
     });
 
     expect(userRegistered).toBeUndefined();
+  });
+
+  it('should return a user when find by cpf with valid cpf', async () => {
+    await typeormUserRepository.save({
+      name: 'new_valid_name',
+      cpf: 'new_valid_cpf',
+      phone: 'new_valid_phone',
+      address: 'new_valid_address',
+      email: 'new_valid_email',
+    });
+
+    const sut = userRepository();
+
+    const findByCpfRegistered = await sut.findByCpf('new_valid_cpf');
+
+    expect(findByCpfRegistered.id).toBeDefined();
+    expect(findByCpfRegistered.cpf).toBe('new_valid_cpf');
   });
 });
