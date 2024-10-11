da8fb95d927e430ca26665c5406a6e2ab3ca2f91
Evandro Calado <evandrocalado07@gmail.com>
2024-10-11 09:19:56 -0300

    test: return true if cpf is registered

--- src/infra/db/typeorm/repositories/user.repository.spec.js
+++ src/infra/db/typeorm/repositories/user.repository.spec.js
@@ -2,46 +2,50 @@ const { typeormServer } = require('../setup');
 const { userRepository, typeormUserRepository } = require('./user.repository');
 
 describe('userRepository', () => {
+  let sut;
+
   beforeEach(async () => {
     await typeormUserRepository.delete({});
   });
 
-  it('should return void when registered a user', async () => {
-    const sut = userRepository();
+  beforeAll(async () => {
+    sut = userRepository();
+  });
 
-    const userRegistered = await sut.register({
-      name: 'valid_name',
-      cpf: 'valid_cpf',
-      phone: 'valid_phone',
-      address: 'valid_address',
-      email: 'valid_email',
-    });
+  const userDTO = {
+    name: 'valid_name',
+    cpf: 'valid_cpf',
+    phone: 'valid_phone',
+    address: 'valid_address',
+    email: 'valid_email',
+  };
+
+  it('should return void when registered a user', async () => {
+    const userRegistered = await sut.register(userDTO);
 
     expect(userRegistered).toBeUndefined();
   });
 
   it('should return a user when find by cpf with valid cpf', async () => {
-    await typeormUserRepository.save({
-      name: 'new_valid_name',
-      cpf: 'new_valid_cpf',
-      phone: 'new_valid_phone',
-      address: 'new_valid_address',
-      email: 'new_valid_email',
-    });
+    await typeormUserRepository.save(userDTO);
 
-    const sut = userRepository();
-
-    const findByCpfRegistered = await sut.findByCpf('new_valid_cpf');
+    const findByCpfRegistered = await sut.findByCpf('valid_cpf');
 
     expect(findByCpfRegistered.id).toBeDefined();
-    expect(findByCpfRegistered.cpf).toBe('new_valid_cpf');
+    expect(findByCpfRegistered.cpf).toBe('valid_cpf');
   });
 
   it('should return null if cpf is not registered', async () => {
-    const sut = userRepository();
-
     const findByCpfRegistered = await sut.findByCpf('not_registered_cpf');
 
     expect(findByCpfRegistered).toBeNull();
+  });
+
+  it('should return true if exists a user with valid cpf', async () => {
+    await typeormUserRepository.save(userDTO);
+
+    const existsByCpf = await sut.existsByCpf('valid_cpf');
+
+    expect(existsByCpf).toBe(true);
   });
 });
